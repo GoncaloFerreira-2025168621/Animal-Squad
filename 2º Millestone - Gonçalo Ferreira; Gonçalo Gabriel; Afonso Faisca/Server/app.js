3dc544baab9f9940ad74a376790f72559b68e9f1
GoncaloFerreira-2025168621 <2025168621@estudantes.ips.pt>
2026-05-20 01:35:52 +0100

    2milestone

---
+++ 2º Millestone - Gonçalo Ferreira; Gonçalo Gabriel; Afonso Faisca/Server/app.js
@@ -0,0 +1,35 @@
+// Importa o express (servidor)
+const express = require("express");
+
+// Permite comunicação entre Unity e Node.js
+const cors = require("cors");
+
+// Importa as funções do request-handlers.js
+const handlers = require("./request-handlers");
+
+// Cria o servidor
+const app = express();
+
+// Permite requests externas
+app.use(cors());
+
+// Permite receber JSON da Unity
+app.use(express.json());
+
+
+//ROUTES
+
+// Route de registo
+app.post("/register", handlers.register);
+
+// Route de login
+app.post("/login", handlers.login);
+
+
+//SERVER 
+
+// Inicia servidor na porta 3000
+app.listen(3000, () => {
+
+    console.log("Servidor online na porta 3000");
+});
---
+++ 2º Millestone - Gonçalo Ferreira; Gonçalo Gabriel; Afonso Faisca/Server/db.js
@@ -0,0 +1,44 @@
+// Importa a biblioteca mysql2
+// Esta biblioteca permite ao Node.js comunicar com o MySQL
+const mysql = require("mysql2");
+
+
+// Cria a ligação à base de dados
+const db = mysql.createConnection({
+
+    // Servidor onde o MySQL está a correr
+    // localhost = o teu próprio PC
+    host: "localhost",
+
+    // Utilizador do MySQL
+    user: "root",
+
+    // Password do utilizador
+    // Neste caso está vazia
+    password: "",
+
+    // Nome da base de dados criada no MySQL Workbench
+    database: "animal_squad"
+});
+
+
+// Tenta ligar à base de dados
+db.connect((err) => {
+
+    // Se existir erro na ligação
+    if (err) {
+
+        console.log("Erro ao ligar à base de dados:");
+        console.log(err);
+
+    } else {
+
+        // Ligação feita com sucesso
+        console.log("MySQL ligado!");
+    }
+});
+
+
+// Exporta a ligação db
+// Assim outros ficheiros podem usar esta ligação
+module.exports = db;
