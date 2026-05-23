9d50b247386dc26c541f7c89bfec1b701db80aac
GoncaloFerreira-2025168621 <2025168621@estudantes.ips.pt>
2026-05-23 11:58:26 +0100

    options

--- Animal Squad/Assets/Scripts/Options.cs
+++ Animal Squad/Assets/Scripts/Options.cs
@@ -7,21 +7,26 @@ public class Options : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        Screen.fullScreen = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Screen.fullScreen = !Screen.fullScreen;
-            Debug.Log("Modo de tela cheia: " + Screen.fullScreen);
-        }
-
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            Screen.SetResolution(1920, 1080, true);
-        }
+        
     }
+
+    public void WindowScreen()
+    {
+        Screen.fullScreenMode = FullScreenMode.Windowed;
+        Debug.Log("Windowed");
+    }
+
+    public void FullScreen()
+    {
+        Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+        Debug.Log("Full Screen");
+    }
+
+
 }
