abb5f7af2fa446c2506a6b7573182d4063e75161
GoncaloFerreira-2025168621 <2025168621@estudantes.ips.pt>
2026-05-24 19:40:23 +0100

    ui

--- Animal Squad/Assets/Scripts/Options.cs
+++ Animal Squad/Assets/Scripts/Options.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 
 public class Options : MonoBehaviour
 {
-
+    [SerializeField] private GameObject _AudioLinha;
+    [SerializeField] private GameObject _VideoLinha;
+    [SerializeField] private GameObject _GameplayLinha;
+    [SerializeField] private GameObject _OptionsMenu;
+    [SerializeField] private GameObject _AudioMenu;
+    [SerializeField] private GameObject _VideoMenu;
+    [SerializeField] private GameObject _GameplayMenu;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,17 +22,81 @@ public class Options : MonoBehaviour
         
     }
 
-    public void WindowScreen()
+    public void BackOptions()//Voltar
     {
-        Screen.fullScreenMode = FullScreenMode.Windowed;
-        Debug.Log("Windowed");
+        _OptionsMenu.SetActive(false);
     }
 
-    public void FullScreen()
+    public void OptionsMenu()//Opções
     {
-        Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
-        Debug.Log("Full Screen");
+        _OptionsMenu.SetActive(true);
     }
+
+    public void Audio()//Audio
+    {
+        _AudioLinha.SetActive(true);
+        _AudioMenu.SetActive(true);
+        _VideoLinha.SetActive(false);
+        _VideoMenu.SetActive(false);
+        _GameplayLinha.SetActive(false);
+        _GameplayMenu.SetActive(false);
+        //_AudioMenu.SetActive(true);
+    }
+
+    public void Video()//Video
+    {
+        _VideoLinha.SetActive(true);
+        _VideoMenu.SetActive(true);
+        _AudioLinha.SetActive(false);
+        _AudioMenu.SetActive(false);
+        _GameplayLinha.SetActive(false);
+        _GameplayMenu.SetActive(false);
+        //_VideoMenu.SetActive(true);
+    }
+    
+    public void Gameplay()//Gameplay
+    {
+        _GameplayLinha.SetActive(true);
+        _GameplayMenu.SetActive(true);
+        _AudioLinha.SetActive(false);
+        _AudioMenu.SetActive(false);
+        _VideoLinha.SetActive(false);
+        _VideoMenu.SetActive(false);
+        //_GameplayMenu.SetActive(true);
+    }
+
+   
+
+public class VideoSettings : MonoBehaviour
+    {
+        public int resolutionIndex;
+        public int qualityIndex;
+        public bool isFullScreen;
+
+        public void WindowScreen()//Janela Normal
+        {
+            Screen.fullScreenMode = FullScreenMode.Windowed;
+            Debug.Log("Windowed");
+        }
+
+        public void MaxWindowScreen()//Janela Maximizada
+        {
+            Screen.fullScreenMode = FullScreenMode.MaximizedWindow;
+            Debug.Log("Maximized Window");
+        }
+
+        public void ExFullScreen()//Tela Cheia Exclusiva
+        {
+            Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+            Debug.Log("Exclusive Full Screen");
+        }
+        public void FullScreen()//Tela Cheia
+        {
+            Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+            Debug.Log("Full Screen");
+        }
+    }
+
 
 
 }
