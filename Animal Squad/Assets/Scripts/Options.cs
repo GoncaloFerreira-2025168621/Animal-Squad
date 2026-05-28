4c5efd4e803570d1cfde006cde4d467815cc1446
GoncaloFerreira-2025168621 <2025168621@estudantes.ips.pt>
2026-05-28 10:05:57 +0100

    video

--- Animal Squad/Assets/Scripts/Options.cs
+++ Animal Squad/Assets/Scripts/Options.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class Options : MonoBehaviour
@@ -14,14 +15,28 @@ public class Options : MonoBehaviour
     void Start()
     {
         Screen.fullScreen = true;
+        _FPSvalidation = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        fps = 1.0f / Time.deltaTime;
+        if (_FPSvalidation == true)
+        {
+            _ShowFPS.text = "FPS: " + Mathf.Round(fps).ToString();
+            _CheckFPSActive.SetActive(true);
+            _CheckFPSInactive.SetActive(false);
+        }
+        else if (_FPSvalidation == false)
+        {
+            _CheckFPSActive.SetActive(false);
+            _CheckFPSInactive.SetActive(true);
+            _ShowFPS.text = "";
+        }
     }
 
+    #region Menu
     public void BackOptions()//Voltar
     {
         _OptionsMenu.SetActive(false);
@@ -64,39 +79,123 @@ public class Options : MonoBehaviour
         _VideoMenu.SetActive(false);
         //_GameplayMenu.SetActive(true);
     }
+    #endregion
 
-   
 
-public class VideoSettings : MonoBehaviour
+    #region Video
+
+    [Header("Screen")]
+    public GameObject _ScreenDropdown;
+    public TextMeshProUGUI _ScreenText;
+
+    [Header("Resolution")]
+    public GameObject _ResolutionDropdown;
+    public TextMeshProUGUI _ResolutionText;
+
+    [Header("FPS")]
+    public TextMeshProUGUI _ShowFPS;
+    public GameObject _CheckFPSActive;
+    public GameObject _CheckFPSInactive;
+    public bool _FPSvalidation;
+    public float fps;
+
+    #region Screen
+    public void DropDownScreen()//Dropdown de Screen
     {
-        public int resolutionIndex;
-        public int qualityIndex;
-        public bool isFullScreen;
-
-        public void WindowScreen()//Janela Normal
-        {
-            Screen.fullScreenMode = FullScreenMode.Windowed;
-            Debug.Log("Windowed");
-        }
-
-        public void MaxWindowScreen()//Janela Maximizada
-        {
-            Screen.fullScreenMode = FullScreenMode.MaximizedWindow;
-            Debug.Log("Maximized Window");
-        }
-
-        public void ExFullScreen()//Tela Cheia Exclusiva
-        {
-            Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
-            Debug.Log("Exclusive Full Screen");
-        }
-        public void FullScreen()//Tela Cheia
-        {
-            Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
-            Debug.Log("Full Screen");
-        }
+        _ScreenDropdown.SetActive(true);
+        
     }
 
+    public void WindowScreen()//Janela Normal
+    {
+        Screen.fullScreenMode = FullScreenMode.Windowed;
+        Debug.Log("Windowed");
+        _ScreenText.text = "Windowed";
+        _ScreenDropdown.SetActive(false);
+    }
 
+    public void MaxWindowScreen()//Janela Maximizada
+    {
+        Screen.fullScreenMode = FullScreenMode.MaximizedWindow;
+        Debug.Log("Maximized Window");
+        _ScreenText.text = "Max Window";
+        _ScreenDropdown.SetActive(false);
+    }
 
+    public void ExFullScreen()//Tela Cheia Exclusiva
+    {
+        Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+        Debug.Log("Ex Full Screen");
+        _ScreenText.text = "Ex Full Screen";
+        _ScreenDropdown.SetActive(false);
+    }
+
+    public void FullScreen()//Tela Cheia
+    {
+        Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+        Debug.Log("Full Screen");
+        _ScreenText.text = "Full Screen";
+        _ScreenDropdown.SetActive(false);
+    }
+    #endregion
+
+    #region Resolution
+    public void DropDownResolution()//Dropdown de Resolução
+    {
+        _ResolutionDropdown.SetActive(true);
+    }
+
+    public void Res720p()//720p
+    {
+        Screen.SetResolution(1280, 720, true);
+        Debug.Log("720p");
+        _ResolutionText.text = "720p";
+        _ResolutionDropdown.SetActive(false);
+    }
+
+    public void Res768p()//768p
+    {
+        Screen.SetResolution(1366, 768, true);
+        Debug.Log("768p");
+        _ResolutionText.text = "768p";
+        _ResolutionDropdown.SetActive(false);
+    }
+
+    public void Res1080p()//1080p
+    {
+        Screen.SetResolution(1920, 1080, true);
+        Debug.Log("1080p");
+        _ResolutionText.text = "1080p";
+        _ResolutionDropdown.SetActive(false);
+    }
+
+    public void Res1440p()//1440p
+    {
+        Screen.SetResolution(2560, 1440, true);
+        Debug.Log("1440p");
+        _ResolutionText.text = "1440p";
+        _ResolutionDropdown.SetActive(false);
+    }
+
+    public void Res2160p()//2160p
+    {
+        Screen.SetResolution(3840, 2160, true);
+        Debug.Log("2160p");
+        _ResolutionText.text = "2160p";
+        _ResolutionDropdown.SetActive(false);
+    }
+    #endregion
+
+    public void ShowFPS()//Mostrar FPS
+    {
+        if (_FPSvalidation == false)
+        {
+            _FPSvalidation = true;
+        }
+        else
+        {
+            _FPSvalidation = false;
+        }
+    }
+    #endregion
 }
